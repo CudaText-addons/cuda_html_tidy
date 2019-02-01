57bbc8d195744b21356d10d10db43f689cfb9815
Alexey-t <support@uvviewsoft.com>
2019-02-01 23:35:42 +0300

    avoid depre API

--- __init__.py
+++ __init__.py
@@ -16,25 +16,28 @@ dir_temp = tempfile.gettempdir()
 
 
 def do_log_clear():
+
     app_log(LOG_CLEAR, '', panel=LOG_PANEL_VALIDATE)
 
 def do_log(fn_ed, fn_err):
+
     app_log(LOG_SET_REGEX, r'line (\d+) column (\d+) .+', panel=LOG_PANEL_VALIDATE)
     app_log(LOG_SET_LINE_ID, '1', panel=LOG_PANEL_VALIDATE)
     app_log(LOG_SET_COL_ID, '2', panel=LOG_PANEL_VALIDATE)
     app_log(LOG_SET_NAME_ID, '0', panel=LOG_PANEL_VALIDATE)
     app_log(LOG_SET_FILENAME, fn_ed, panel=LOG_PANEL_VALIDATE)
-    
+
     text = open(fn_err).read().splitlines()
     if not text: return
     for s in text:
         app_log(LOG_ADD, s, panel=LOG_PANEL_VALIDATE)
-        
-    ed.focus()    
+
+    ed.focus()
     ed.cmd(cudatext_cmd.cmd_ShowPanelValidate)
 
 
 def do_menu():
+
     l = os.listdir(config_dir)
     if not l: return
     l = sorted(l)
@@ -46,41 +49,39 @@ def do_menu():
 
 
 def do_tidy(validate_only):
-    if app_api_version()<'1.0.102':
-        msg_box('Need newer app version', MB_OK+MB_ICONERROR)
-        return
 
     fn_ed = ed.get_filename()
     if not fn_ed:
         msg_status('Save file first')
         return
-        
-    file_save()
+
+    if ed.get_prop(PROP_MODIFIED):
+        ed.save()
 
     if not validate_only:
         fn_cfg = do_menu()
         if not fn_cfg:
             return
-                             
+
     fn_out = os.path.join(dir_temp, 'tidy_out.txt')
     fn_err = os.path.join(dir_temp, 'tidy_err.txt')
     if os.path.isfile(fn_out):
         os.remove(fn_out)
     if os.path.isfile(fn_err):
         os.remove(fn_err)
-    
+
     if validate_only:
         command = [fn_exe, '-file', fn_err, '-errors', '-quiet', fn_ed]
     else:
         command = [fn_exe, '-output', fn_out, '-config', fn_cfg, '-file', fn_err, '-quiet', fn_ed]
-    
-    try:     
+
+    try:
         do_run_hide(command)
     except:
         msg_box('Cannot run command "tidy", install it first', MB_OK+MB_ICONERROR)
-        return    
+        return
     do_log_clear()
-        
+
     if os.path.isfile(fn_err) and open(fn_err).read():
         msg_status('Tidy returned error')
         do_log(fn_ed, fn_err)
@@ -104,17 +105,22 @@ def do_tidy(validate_only):
 
 
 class Command:
+
     def menu(self):
+
         do_tidy(False)
 
     def validate(self):
+
         do_tidy(True)
 
     def configs(self):
+
         if os.name=='nt':
             do_run_normal(['Explorer.exe', config_dir])
         else:
             msg_box('Command is for Windows only', MB_OK)
 
     def web(self):
+
         webbrowser.open_new_tab(help_url)
